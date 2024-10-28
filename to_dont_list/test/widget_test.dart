07751e611361a0dfe8f6163457698e33c9e257fa
eso14 <eliana.oden14@gmail.com>
2024-10-28 10:12:55 -0500

    widgwt_test for murph branch

--- to_dont_list/test/widget_test.dart
+++ to_dont_list/test/widget_test.dart
@@ -20,20 +20,6 @@ void main() {
   });
 
   // Yes, you really need the MaterialApp and Scaffold
-  testWidgets('ToDoListItem has a text', (tester) async {
-    await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-            body: ContactListItems(
-                item: const Contact(first_name: "test", last_name:"last", number: "01" ),
-                favorited: true,
-                onListChanged: (Contact item, bool favorited) {},
-                onDeleteItem: (Contact item) {}))));
-    final textFinder = find.text("test");
-
-    // Use the `findsOneWidget` matcher provided by flutter_test to verify
-    // that the Text widgets appear exactly once in the widget tree.
-    expect(textFinder, findsOneWidget);
-  });
 
   testWidgets('ContactItem has a Circle Avatar with abbreviation',
       (tester) async {
@@ -63,29 +49,6 @@ void main() {
 
     expect(listItemFinder, findsOneWidget);
   });
-
-  testWidgets('Clicking and Typing adds Contact to ContactList', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: ToDoList()));
-
-    expect(find.byType(TextField), findsNothing);
-
-    await tester.tap(find.byKey(Key("Add")));
-    await tester.pump(); // Pump after every action to rebuild the widgets
-    expect(find.text("hi"), findsNothing);
-
-    await tester.enterText(find.byType(TextField), 'hi');
-    await tester.pump();
-    expect(find.text("hi"), findsOneWidget);
-
-    await tester.tap(find.byKey(const Key("OKButton")));
-    await tester.pump();
-    expect(find.text("hi"), findsOneWidget);
-
-    final listItemFinder = find.byType(ContactListItems);
-
-    expect(listItemFinder, findsNWidgets(2));
-  });
-
   testWidgets('Favorite icon changes when clicked',
       (tester) async {
     await tester.pumpWidget(MaterialApp(
